daa5a868b2cfd25ef49b685b328d11c72d5da2e2
Toke Jepsen <tokejepsen@gmail.com>
2017-06-09 11:02:47 +0200

    Expose getting the new api structure.

--- ftrack_locations/__init__.py
+++ ftrack_locations/__init__.py
@@ -18,3 +18,11 @@ def get_new_location(session):
 
     location = __import__(module).get_new_location(session)
     return location
+
+
+def get_new_structure():
+
+    module = os.environ.get("FTRACK_LOCATIONS_MODULE", "")
+    sys.path.append(os.path.dirname(__file__))
+
+    return __import__(module).NewStructure()
